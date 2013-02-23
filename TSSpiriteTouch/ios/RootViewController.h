470783cf1a54eac46d8e3d89169037d742b5479c
Enel <spiritring@gmail.com>
2013-02-23 12:38:36 +0800

    Initial Commit

---
+++ TSSpiriteTouch/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TSSpiriteTouchAppController.h
+//  TSSpiriteTouch
+//
+//  Created by TSEnel on 13-2-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
